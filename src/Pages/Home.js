c04e1621f6112fa2aa3228ca2f1978049abeeeac
jaffrantirta <jaffrantirta@icloud.com>
2023-04-27 11:02:54 +0800

    explain section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Hero from '../Sections/Hero'
 import Carousel from '../Sections/Carousel'
+import Explain from '../Sections/Explain'
 
 export default function Home() {
     return (
         <div>
             <Hero />
             <Carousel />
+            <Explain
+                title='Ad aute laborum veniam laboris tempor cillum esse ipsum veniam quis adipisicing do laboris.'
+                paragraph='Id tempor id irure ad est irure tempor adipisicing nostrud voluptate amet sunt velit. Laborum cillum dolore ad eu enim aute anim excepteur ex non id duis. Ipsum est minim Lorem qui quis enim qui officia sint.'
+                bg='https://static01.nyt.com/images/2017/02/03/arts/03TATTOOJP3/03TATTOOJP3-articleInline.jpg?quality=75&auto=webp&disable=upscale'
+            />
+            <Explain
+                isFlip={true}
+                title='Ad aute laborum veniam laboris tempor cillum esse ipsum veniam quis adipisicing do laboris.'
+                paragraph='Id tempor id irure ad est irure tempor adipisicing nostrud voluptate amet sunt velit. Laborum cillum dolore ad eu enim aute anim excepteur ex non id duis. Ipsum est minim Lorem qui quis enim qui officia sint.'
+                bg='https://static01.nyt.com/images/2017/02/03/arts/03TATTOOJP3/03TATTOOJP3-articleInline.jpg?quality=75&auto=webp&disable=upscale'
+            />
         </div>
     )
 }
---
+++ src/Sections/Explain.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Explain(props) {
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2 h-fit'>
+            <div style={{ backgroundImage: `url("${props.bg}")` }} className={`bg-cover aspect-square bg-center bg-no-repeat ${props.isFlip ? 'md:block hidden' : 'hidden'}`}></div>
+            <div className='text-center p-10 flex flex-col justify-center'>
+                <h1 className='text-2xl md:text-4xl mb-5 font-bold'>{props.title}</h1>
+                <p>{props.paragraph}</p>
+            </div>
+            <div style={{ backgroundImage: `url("${props.bg}")` }} className={`bg-cover aspect-square bg-center bg-no-repeat ${props.isFlip ? 'md:hidden' : ''}`}></div>
+        </div>
+
+    )
+}
