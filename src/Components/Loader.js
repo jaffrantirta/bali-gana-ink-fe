51fd3fd8bc75d5a0b54f88f9a8618e91b6137fdc
jaffrantirta <jaffrantirta@icloud.com>
2023-05-11 17:11:35 +0800

    update loader

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loader() {
     return (
-        <div className='w-full h-full flex justify-center m-10'>
+        <div className='w-full h-full flex justify-center'>
             <div className='bg-slate-500 w-16 h-16 rounded-3xl animate-spin'></div>
         </div>
     )
